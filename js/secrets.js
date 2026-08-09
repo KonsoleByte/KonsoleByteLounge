035d3fb036808adc9636a23038eeb3b33cb7e26f
KonsoleByte <79225265+KonsoleByte@users.noreply.github.com>
2026-08-09 00:09:37 -0700

    Add files via upload

---
+++ js/secrets.js
@@ -0,0 +1,12 @@
+let t =  "";
+
+document.addEventListener("keydown", (event) => {
+    t += event.key.toLowerCase()
+
+    if (t.length > 9) {
+        t = t.slice(-9);
+    }
+    if (t === "deltarune") {
+        document.getElementById("style").href = "css/deltarune.css";
+    }
+})
