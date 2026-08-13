df8aced1bc7a05769c4e6d54b95153ce54326f92
KonsoleByte <79225265+KonsoleByte@users.noreply.github.com>
2026-08-13 02:47:10 -0700

    Initial Web Finish

---
+++ js/egg.js
@@ -0,0 +1,31 @@
+const image = document.getElementById("eggImage");
+
+let clicked = false;
+
+image.addEventListener("click", function () {
+    if (clicked) return;
+    clicked = true;
+
+    const openSound = new Audio("assets/open.mp3");
+
+    openSound.play().catch(error => {
+        console.error("Door sound failed:", error);
+    });
+
+    const eggSong = new Audio("assets/eggsong.mp3");
+
+    eggSong.loop = true;
+    eggSong.volume = 1.0;
+
+    eggSong.load();
+
+    eggSong.play()
+
+    image.style.display = "none";
+
+    setTimeout(() => {
+        image.src = "assets/tree.png";
+        image.alt = "Tree";
+        image.style.display = "block";
+    }, 1000);
+});
