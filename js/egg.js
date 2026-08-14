24ffc624e29fb6c8ca26ad64f3758f8681ad9857
KonsoleByte <79225265+KonsoleByte@users.noreply.github.com>
2026-08-14 01:48:15 -0700

    Add files via upload

--- js/egg.js
+++ js/egg.js
@@ -5,24 +5,16 @@ let clicked = false;
 image.addEventListener("click", function () {
     if (clicked) return;
     clicked = true;
-
     const openSound = new Audio("assets/open.mp3");
-
     openSound.play().catch(error => {
         console.error("Door sound failed:", error);
     });
-
     const eggSong = new Audio("assets/eggsong.mp3");
-
     eggSong.loop = true;
     eggSong.volume = 1.0;
-
     eggSong.load();
-
     eggSong.play()
-
     image.style.display = "none";
-
     setTimeout(() => {
         image.src = "assets/tree.png";
         image.alt = "Tree";
--- js/secrets.js
+++ js/secrets.js
@@ -2,7 +2,6 @@ let t =  "";
 
 document.addEventListener("keydown", (event) => {
     t += event.key.toLowerCase()
-
     if (t.length > 9) {
         t = t.slice(-9);
     }
